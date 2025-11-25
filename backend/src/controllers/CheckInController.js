e63e9733df154a10d5c71dbccab034da0ff2095f
Guilherme Montenegro <guilhermeu028@gmail.com>
2025-11-25 18:38:13 -0300

    feat(CheckInController): conected to conecta API

--- backend/src/controllers/CheckInController.js
+++ backend/src/controllers/CheckInController.js
@@ -1,4 +1,7 @@
 const checkInService = require('../services/CheckInService');
+const conecta = require("../services/ConectaAPI");
+const usuarioService = require("../services/UsuarioService");
+const eventoService = require("../services/EventoService");
 
 class CheckInController {
 
@@ -8,7 +11,31 @@ class CheckInController {
 
             const resultado = await checkInService.realizarCheckIn(usuarioId, Number(eventoId));
 
-            return resultado;
+            const usuario = await usuarioService.obterPorId(usuarioId);
+            const evento  = await eventoService.obterPorId(eventoId);
+
+            await conecta.autenticar(usuario.cpf, usuario.senha);  
+
+                const dados = {
+                    userIdentifier: usuario.cpf,   // obrigatório
+                    eventName: evento.nome,        // obrigatório
+                    checkInDateTime: new Date().toISOString(),
+                    cidade: "Recife",              // vocês podem pegar via Evento também
+                    bairro: "Centro",
+                    rua: evento.local,
+                    identifier: `EVENTO-${eventoId}-USER-${usuarioId}`,
+                    document: usuario.cpf
+            };
+
+
+            const respostaConecta = await conecta.fazerCheckIn(dados);
+
+
+            return {
+                checkInLocal: resultado,
+                conecta: respostaConecta
+            };
+
 
         } catch (error) {
             console.error("Erro no Check-in:", error.message);
