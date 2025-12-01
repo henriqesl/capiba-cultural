0b7edc9f7e4c2774832c921948d44d31423a0812
Mateus <mateusburgos50@gmail.com>
2025-12-01 03:22:35 -0300

    fix(images): correction to image generation on the login and profile pages

--- frontend/src/components/CapibaLogo.jsx
+++ frontend/src/components/CapibaLogo.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import capiba_logo from '../assets/logo_capiba.png'
+import capiba_logo from '../assets/logo_capiba.png';
 
 const CapibaLogo = () => (
     <img
         src={capiba_logo}
         alt="Logo da Moeda Capiba"
         className="h-32 w-32 rounded-full object-cover shadow-lg"
-        onError={(e) => { e.target.onerror = null; e.target.src='https://placehold.co/128x128/cccccc/ffffff?text=Erro'; }}
     />
 );
 
